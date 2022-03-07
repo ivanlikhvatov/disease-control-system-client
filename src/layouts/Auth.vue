87bb70e57023aeb829922751e8157bf8363f38c8
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-03-07 15:18:37 +0400

    initial commit

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container  mt-10>
+
+    <v-row justify="space-around">
+      <v-card width="400">
+        <v-card-title>
+          <p style="font-size: x-large; font-weight: bold; color: slategrey">Пожалуйста авторизируйтесь</p>
+        </v-card-title>
+
+        <v-card-text v-if="infoMessage">
+          <p style="font-size: small; font-weight: bold; color: brown">{{infoMessage}}</p>
+        </v-card-text>
+
+        <v-card-text class="justify-center">
+          <form action="/login" method="post">
+            <v-text-field
+                name="email"
+                label="E-mail"
+                required
+
+            ></v-text-field>
+
+            <v-text-field
+                name="password"
+                label="Пароль"
+                required
+
+            ></v-text-field>
+
+            <v-btn
+                color="info"
+                class="mr-4"
+                type="submit"
+            >
+              Войти
+            </v-btn>
+
+            <v-btn
+                color="info"
+                class="mr-4"
+                @click="showRegistrationForm"
+            >
+              Зарегистрироваться
+            </v-btn>
+          </form>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HomeView'
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
+  vuetify : new Vuetify(),
   router,
   store,
   render: h => h(App)
